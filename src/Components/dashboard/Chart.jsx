df4d461ce9d13e9fe932b44b5409af39830cba05
Shreedhar Bhat <shridharbhat1998@gmail.com>
2020-09-01 15:04:28 +0530

    Pie chart added

---
+++ src/Components/dashboard/Chart.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "./Chart.module.css";
+
+function Chart() {
+    return (
+        <div className={styles.piechart}></div>
+    )
+}
+
+export default Chart
